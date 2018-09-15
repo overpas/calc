637f204541a63b9cbe26ecb38343a7b7ca39625f
overpas <pckeycalculator@gmail.com>
2018-09-15 10:59:02 +0300

    Small fixes

--- app/src/main/java/by/overpass/poms1/ui/main/activity/MainActivity.kt
+++ app/src/main/java/by/overpass/poms1/ui/main/activity/MainActivity.kt
@@ -1,7 +1,6 @@
 package by.overpass.poms1.ui.main.activity
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import by.overpass.poms1.R
